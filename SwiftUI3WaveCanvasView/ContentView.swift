3f7ebac8fedcf0895627ce7fa44239269e594126
张亚飞 <672233580@qq.com>
2021-06-13 19:34:11 +0800

    Initial Commit

---
+++ SwiftUI3WaveCanvasView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI3WaveCanvasView
+//
+//  Created by 张亚飞 on 2021/6/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI3WaveCanvasView/SwiftUI3WaveCanvasViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI3WaveCanvasViewApp.swift
+//  SwiftUI3WaveCanvasView
+//
+//  Created by 张亚飞 on 2021/6/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI3WaveCanvasViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
